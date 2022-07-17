732b511564c4c1c75ca04c8fa22ef2c9675e369d
root <root@00bb19993297>
2022-07-17 19:57:00 +0000

    I worked with 2d arrays!

--- src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -7,11 +7,11 @@ public class CrazyDigitalPainting {
 	final static int width = 500;
 	final static int height = 500;
 
-    // 2. Create a 2D array of Color objects. You will need to import
+     // 2. Create a 2D array of Color objects. You will need to import
     //    java.awt.Color. Initialize the size of the array using the 
     //    integers created in step 1.
 	
-	Color[][] colors = new Color[width][height];
+	Color[][] colors = new Color[height][width];
 	
 	
 
@@ -23,11 +23,15 @@ public class CrazyDigitalPainting {
         //    to a new color. The sample image was created using the following 
         //    pattern:
         //    colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-
+    	for(int i = 0; i < colors.length; i++) {
+    		for(int j = 0; j < colors[i].length; j++) {
+    			colors[i][j] = new Color(i % 256, (i * j) % 256, (j * j) % 256);
+    		}
+    	}
         // 5. Come up with your own pattern to make a cool crazy image.
-
         // 6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
         //    to show off your picture.
+    	ColorArrayDisplayer.displayColorsAsImage(colors);
     }
 
     public static void main(String[] args) {
